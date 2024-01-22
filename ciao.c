4a3517e5f7ff87413df173779b6a5dbbc7362479
lucreziamorbinati <157354213+lucreziamorbinati@users.noreply.github.com>
2024-01-22 16:00:07 +0000

    .

--- ciao.c
+++ ciao.c
@@ -1,1 +1,10 @@
-teg
+#include <stdio.h>
+int main()
+{
+    int wow = 13;
+
+ /*questo programma stamperà una stringa*/
+    printf("%d", wow);
+    return(0);
+}
+   
---
+++ new.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{ 
+   char a;
+
+    printf("inserisci una lettera\n");
+    scanf("%c", &a);
+    printf("a= %c", a);
+
+
+}
